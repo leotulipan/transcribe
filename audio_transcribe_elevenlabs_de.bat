674a300c2553a1cc4b1c589ff65ea79b12262977
Leonard Tulipan <leo@leotulipan.at>
2025-04-30 08:29:11 +0200

    feat: batch files, icons and links for easy drag and drop usage

---
+++ audio_transcribe_elevenlabs_de.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set "filename=%~1"
+cd /d "%~dp1"
+
+uv run --link-mode=copy C:\Users\leona\OneDrive\_2_Areas\Scripts.Transcribe\audio_transcribe_elevenlabs_adapted.py -d -v -s -D --padding 50 -p 350 "%filename%"
+
+pause 
