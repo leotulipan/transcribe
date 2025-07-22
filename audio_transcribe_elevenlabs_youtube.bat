9db7bc96a5ff63addc2a6088ee92b2cac9fa2470
Leonard Tulipan <leo@leotulipan.at>
2025-07-22 12:21:13 +0200

    feat: add batch script for YOUTUBE Subtitles using ElevenLabs

---
+++ audio_transcribe_elevenlabs_youtube.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set "filename=%~1"
+cd /d "%~dp1"
+
+REM Call the unified transcribe.py script with ElevenLabs API and German language
+uv run --link-mode=copy "%~dp0\transcribe.py" --file "%filename%" --api elevenlabs --language de --verbose 
+
+pause 
